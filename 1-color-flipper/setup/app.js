f368d261e4aa4ed9f9fbc7b5858ac6c8ba233e0f
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-11-21 10:30:40 -0600

    finish color picker

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,25 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// Constants
+
+let currentColorIdx = 0;
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+// Elements
+
+const buttonElement = document.querySelector('#btn');
+const mainElement = document.querySelector('main');
+
+// Functions
+
+const changeColor = () => {
+  if (currentColorIdx === colors.length - 1) {
+    currentColorIdx = 0;
+  } else {
+    currentColorIdx += 1;
+  }
+
+  mainElement.style.backgroundColor = `${colors[currentColorIdx]}`;
+};
+
+// Event Listeners
+
+buttonElement.addEventListener('click', changeColor);
--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,25 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+// Constants
+
+const buttonElement = document.querySelector('#btn');
+const mainElement = document.querySelector('main');
+
+// Event Listeners
+
+buttonElement.addEventListener('click', getNewColor);
+
+// Functions
+
+function getNewColor() {
+  const newHex = [];
+  for (let i = 0; i < 6; i++) {
+    newHex.push(hex[getRandomNumber()]);
+  }
+
+  mainElement.style.backgroundColor = `#${newHex.join('')}`;
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
