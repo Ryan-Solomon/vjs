57361d107db882ff7691b1809e2a6c6492d6b8de
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-11-21 15:30:10 -0600

    add show and hide of elements

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,29 @@
-//using selectors inside the element
-// traversing the dom
+// Elements
+const plusIcons = document.querySelectorAll('.plus-icon');
+const minusIcons = document.querySelectorAll('.minus-icon');
+const questionTextBlocks = document.querySelectorAll('.question-text');
+const questionsSection = document.querySelector('.questions');
+
+// Event Listeners
+
+questionsSection.addEventListener('click', hideOrShowAnswer);
+
+// Functions
+
+function hideOrShowAnswer(event) {
+  if (event.target.classList.contains('fa-plus-square')) {
+    const questionBlock =
+      event.target.parentElement.parentElement.parentElement.parentElement;
+    questionBlock.classList.add('show-text');
+
+    questionTextBlocks.forEach((block) => {
+      if (block.parentElement.id !== questionBlock.id) {
+        block.parentElement.classList.remove('show-text');
+      }
+    });
+  } else if (event.target.classList.contains('fa-minus-square')) {
+    event.target.parentElement.parentElement.parentElement.parentElement.classList.remove(
+      'show-text'
+    );
+  }
+}
