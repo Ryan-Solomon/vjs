4da7062730a36f58c4716f5dba7ddfe4e0b84601
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-11-21 14:53:17 -0600

    add open and show modal

--- 6-modal/setup/app.js
+++ 6-modal/setup/app.js
@@ -1,4 +1,19 @@
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+// Elements
+const openModalButton = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeModalButton = document.querySelector('.close-btn');
+
+// Event Listeners
+
+openModalButton.addEventListener('click', openModal);
+closeModalButton.addEventListener('click', closeModal);
+
+// Functions
+
+function openModal() {
+  modalOverlay.classList.add('open-modal');
+}
+
+function closeModal() {
+  modalOverlay.classList.remove('open-modal');
+}
